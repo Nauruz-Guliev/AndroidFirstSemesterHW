3db146109d1c6866948238b5c52028e8a70fc81b
Nauruz-Guliev <nauruz9248327@gmail.com>
2022-12-14 22:47:50 +0300

    init

--- app/src/main/java/ru/kpfu/itis/hw_android_2022/MainActivity.kt
+++ app/src/main/java/ru/kpfu/itis/hw_android_2022/MainActivity.kt
@@ -2,10 +2,40 @@ package ru.kpfu.itis.hw_android_2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import ru.kpfu.itis.hw_android_2022.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpNavigation()
+
+    }
+
+    override fun onSupportNavigateUp() = findNavController(R.id.fragment_container).navigateUp()
+
+    private fun setUpNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(R.id.firstFragment, R.id.secondFragment),
+            fallbackOnNavigateUpListener = ::onSupportNavigateUp
+        )
+        binding.bottomNav.setupWithNavController(navController)
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(
+            navController = navController,
+            configuration = appBarConfiguration
+        )
     }
 }
---
+++ app/src/main/java/ru/kpfu/itis/hw_android_2022/fragments/FirstFragment.kt
@@ -0,0 +1,74 @@
+package com.example.androidclass.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
+import com.example.androidclass.viewpager.ViewPagerAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import ru.kpfu.itis.hw_android_2022.databinding.FragmentFirstBinding
+import ru.kpfu.itis.hw_android_2022.load
+import ru.kpfu.itis.hw_android_2022.preloadImage
+
+class FirstFragment : Fragment() {
+    private var _binding: FragmentFirstBinding? = null
+    private val binding: FragmentFirstBinding get() = _binding!!
+
+    private val glide by lazy { Glide.with(this) }
+    private val adapter = ViewPagerAdapter()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+
+        initAdapter()
+        with(binding) {
+            btnPreload.setOnClickListener {
+                preload(adapter.imageUrl.toList())
+            }
+        }
+
+        return binding.root
+    }
+
+    private fun initAdapter() {
+        adapter.onWork = ::someWork
+        binding.viewpager.adapter = adapter
+    }
+
+    private fun someWork(url: String, imageView: ImageView, pb: ProgressBar) {
+        lifecycleScope.launch(Dispatchers.IO) {
+            delay(INITIAL_DELAY)
+            glide.preloadImage(url)
+            withContext(Dispatchers.Main) {
+                imageView.load(url, glide, pb)
+            }
+        }
+    }
+
+    private fun preload(list: List<String>) {
+        lifecycleScope.launch(Dispatchers.IO) {
+            list.chunked(list.size / CHUNK_SIZE).forEach { chunkedPart ->
+                launch {
+                    chunkedPart.forEach {
+                        glide.preloadImage(it)
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val INITIAL_DELAY = 2000L
+        private const val CHUNK_SIZE = 3
+    }
+}
